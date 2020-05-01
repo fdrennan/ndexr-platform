cc83bfa2a1d7dcb051567336bb017a83f5d59448
Freddy Drennan <drennanfreddy@gmail.com>
2020-04-30 21:55:57 -0600

    update

--- airflower/scripts/R/r_files/r_venv_install.R
+++ airflower/scripts/R/r_files/r_venv_install.R
@@ -9,6 +9,8 @@ VIRTUALENV_NAME <- 'redditor'
 virtualenv_install(envname = VIRTUALENV_NAME, packages = 'praw')
 install_python(method = 'virtualenv', envname = VIRTUALENV_NAME)
 
-system(
-  new_glue('echo RETICULATE_PYTHON=${HOME}/.virtualenvs/--VIRTUALENV_NAME--/bin/python >> .Renviron')
-)
+if (Sys.getenv('RETICULATE_PYTHON') != '') {
+    system(
+      new_glue('echo RETICULATE_PYTHON=${HOME}/.virtualenvs/--VIRTUALENV_NAME--/bin/python >> .Renviron')
+    )
+}
