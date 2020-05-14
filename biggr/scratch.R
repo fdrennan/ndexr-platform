34a5d0619fd8c402d1de0bca9b89419a51d90126
Freddy Drennan <drennanfreddy@gmail.com>
2020-05-14 15:57:54 -0600

    update

---
+++ biggr/scratch.R
@@ -0,0 +1,24 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: fdrennan
+# Created on: 5/14/20
+
+
+
+library(biggr)
+library(ipify)
+
+sgd <- security_group_data()
+
+ips <- sgd %>%
+  filter(group_name == 'Router', from_port == 22)
+
+current_ip <-
+ ips %>%
+   filter(str_detect(ip_ranges, my_ip))
+
+if(nrow(current_ip) == 0) {
+  security_group_revoke(sg_name = 'Router', ports = 22, ips = str_remove_all(ips$ip_ranges, "/32"))
+  my_ip = get_ip()
+  security_group_envoke(sg_name = 'Router', ports = 22, ips = my_ip)
+}
