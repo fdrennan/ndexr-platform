45c36e7c9803d8479c6536f5774642634ec4d41c
Freddy Drennan <drennanfreddy@gmail.com>
2020-05-14 21:32:08 -0600

    update

--- redditor-api/elasticsubmissions.R
+++ redditor-api/elasticsubmissions.R
@@ -6,51 +6,53 @@ con = postgres_connector()
 stream_submissions_all <- tbl(con, in_schema('public', 'stream_submissions_all'))
 
 counts <-
-  stream_submissions_all %>% 
+  stream_submissions_all %>%
   mutate(
     year = date_part('year', created_utc),
     month = date_part('month', created_utc),
     day = date_part('day', created_utc),
     hour = date_part('hour', created_utc),
   ) %>%
-  filter(created_utc <= local(floor_date(now(tzone = 'UTC') - hours(2), 'hour'))) %>% 
-  distinct(year, month, day, hour) %>% 
-  arrange(year, month, day, hour) %>% 
-  as.data.frame() %>% 
+  filter(created_utc <= local(floor_date(now(tzone = 'UTC') - hours(2), 'hour'))) %>%
+  distinct(year, month, day, hour) %>%
+  arrange(year, month, day, hour) %>%
+  as.data.frame() %>%
   mutate(id = row_number())
 
 if (db_has_table(con,'elastic_uploaded_submissions')) {
   elastic_uploaded <- collect(tbl(con, in_schema('public', 'elastic_uploaded_submissions')))
-  counts <- anti_join(counts, elastic_uploaded)
+  counts <- anti_join(counts, elastic_uploaded) %>%
+  mutate(id = row_number())
 }
 
 
 #elastic("http://localhost:9200", "stream_submissions_all") %delete% TRUE
 #elastic("http://localhost:9200", "stream_submissions_all") %delete% TRUE
 max_counts = nrow(counts)
+es = elastic(Sys.getenv('ELASTIC_SEARCH'), "stream_submissions_all", "data")
 
-counts %>% 
-  split(.$id) %>% 
+counts %>%
+  split(.$id) %>%
   map(
     function(x) {
       print(x$id/max_counts)
-      response <- 
-        stream_submissions_all %>% 
+      response <-
+        stream_submissions_all %>%
         mutate(
           year = date_part('year', created_utc),
           month = date_part('month', created_utc),
           day = date_part('day', created_utc),
           hour = date_part('hour', created_utc),
-        ) %>% 
+        ) %>%
         filter(
           year == local(x$year),
           month == local(x$month),
           day == local(x$day),
           hour == local(x$hour)
-        ) %>% 
+        ) %>%
         collect
       print(response)
-      elastic("http://localhost:9200", "stream_submissions_all", "data") %index% response
+      es %index% response
       dbWriteTable(conn  = con, name = 'elastic_uploaded_submissions', value = x, append = TRUE)
     }
   )
