2e731e2d5c64de91e8476585029f55355a115142
Freddy Drennan <drennanfreddy@gmail.com>
2020-05-29 12:31:35 -0600

    update

--- airflow/airflower/scripts/R/r_files/backup_postgres_to_s3.R
+++ airflow/airflower/scripts/R/r_files/backup_postgres_to_s3.R
@@ -3,5 +3,5 @@ library(redditor)
 
 setwd('/')
 tar("postgres.tar.gz", "postgres.bak", compression = "gzip", tar = "tar")
-s3_upload_file(bucket = "reddit-dumps", from = "postgres.tar.gz", to = "postgres.tar.gz", make_public = TRUE)
+s3_upload_file(bucket = "reddit-dumps", from = "/postgres.tar.gz", to = "postgres.tar.gz", make_public = TRUE)
 file_delete("/postgres.tar.gz")
