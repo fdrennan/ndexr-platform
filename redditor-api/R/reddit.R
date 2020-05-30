4124d48f7e60f2a3744b64b2524aef7486c96196
Freddy Drennan <drennanfreddy@gmail.com>
2020-05-30 17:26:09 -0600

    update

--- redditor-api/R/reddit.R
+++ redditor-api/R/reddit.R
@@ -555,33 +555,15 @@ get_submission <- function(reddit = NULL, name = NULL, type = NULL, limit = 10) 
 
 gather_submissions <- function(con = con, reddit_con = NULL, sleep_time = 3) {
   while (TRUE) {
-    send_message("----------Gathering submissions---------")
     get_all <- get_submission(reddit = reddit_con, name = "all", limit = 3000L, type = "new")
-    # prior <- count_submissions()
-    send_message("Uploading submissions...")
     tryCatch(
       {
         dbxUpsert(con, "submissions", get_all, where_cols = c("submission_key"))
       },
       error = function(e) {
-        message("Something went wrong with upsert in gather_submissions")
+        message("Something went wrong with upsert in gather_submissions, {as.character(Sys.time())}")
       }
     )
-    # after <- count_submissions()
-    send_message("Checking submission counts...")
-    # new_submissions <- after$n_obs - prior$n_obs
-    # downloaded_rows <- nrow(get_all)
-    # overlap_ratio <- round(1 - new_submissions / downloaded_rows, 2)
-    # if (overlap_ratio < .4) {
-    #  sleep_time <- sleep_time - 2
-    #  sleep_time <- max(0, sleep_time)
-    #  send_message(glue("Updating sleep_time to {sleep_time}"))
-    # } else if (overlap_ratio > .6) {
-    #  sleep_time <- sleep_time + 2
-    #  sleep_time <- min(20, sleep_time)
-    #  send_message(glue("Updating sleep_time to {sleep_time}"))
-    # }
-    # send_message(glue("Sleep Time: {sleep_time} -- Downloaded rows: {downloaded_rows} -- New submissions in table: {new_submissions} -- Overlap Ratio: {overlap_ratio}"))
     Sys.sleep(sleep_time)
   }
 }
