f5eaf7b7c5a8bf61e11e3eba88b2228498c5dc5b
Freddy Drennan <drennanfreddy@gmail.com>
2020-06-15 15:13:20 -0600

    update

--- redditor-api/api/plumber.R
+++ redditor-api/api/plumber.R
@@ -4,6 +4,10 @@ library(plumber)
 library(redditor)
 library(biggr)
 
+message('Configuring AWS')
+message(Sys.getenv("AWS_ACCESS"))
+message(Sys.getenv("AWS_SECRET"))
+message(Sys.getenv("AWS_REGION"))
 configure_aws(
   aws_access_key_id = Sys.getenv("AWS_ACCESS"),
   aws_secret_access_key = Sys.getenv("AWS_SECRET"),
