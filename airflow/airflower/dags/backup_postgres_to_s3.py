6dc67a2568387d25016db512c1f6c4f456b0419f
Freddy Drennan <drennanfreddy@gmail.com>
2020-06-22 14:11:10 -0600

    update

--- airflow/airflower/dags/backup_postgres_to_s3.py
+++ airflow/airflower/dags/backup_postgres_to_s3.py
@@ -25,17 +25,17 @@ task_1 = BashOperator(
     dag=dag
 )
 
-task_2 = BashOperator(
-    task_id='backup_postgres',
-    bash_command='. /home/scripts/shell/backup_postgres',
-    dag=dag
-)
+# task_2 = BashOperator(
+#     task_id='backup_postgres',
+#     bash_command='. /home/scripts/shell/backup_postgres',
+#     dag=dag
+# )
 
-task_3 = BashOperator(
+task_2 = BashOperator(
     task_id='backup_postgres_to_s3',
     bash_command='. /home/scripts/R/shell/backup_postgres_to_s3',
     dag=dag
 )
 
 task_1 >> task_2
-task_2 >> task_3
+# task_2 >> task_3
