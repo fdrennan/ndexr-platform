22f6bbce1c90ac682cdf17f85485b0277f6db9af
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-03 11:06:45 -0600

    Update poweredge endpoint

--- redditor-shiny/redditor-api/api/plumber.R
+++ redditor-shiny/redditor-api/api/plumber.R
@@ -99,6 +99,16 @@ function(table_name = "meta_statistics") {
       table_name = table_name
     )
   )
+  
+  get_summary <- function(table_name = "meta_statistics") {
+    con <- postgres_connector(POSTGRES_PORT = 5432, POSTGRES_HOST = Sys.getenv('POWEREDGE'))
+    on.exit(dbDisconnect(conn = con))
+    
+    table_name <- tbl(con, in_schema("public", table_name)) %>%
+      collect()
+    
+    table_name
+  }
 
   response <- tryCatch(
     {
