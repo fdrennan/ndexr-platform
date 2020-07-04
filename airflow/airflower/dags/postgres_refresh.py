8c556d9add1352e834e228ed2dbc0b2c6d3cee8c
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-04 11:13:08 -0600

    Add poweredge update

--- airflow/airflower/dags/postgres_refresh.py
+++ airflow/airflower/dags/postgres_refresh.py
@@ -47,8 +47,14 @@ task_3 = BashOperator(
     dag=dag
 )
 
+task_4 = BashOperator(
+    task_id='refresh_mat_meta_statistics_poweredge',
+    bash_command='. /home/scripts/R/shell/refresh_mat_meta_statistics_poweredge',
+    dag=dag
+)
 
 task   >> task_0
 task_0 >> task_1
 task_0 >> task_2
 task_0 >> task_3
+task_0 >> task_4
---
+++ airflow/airflower/scripts/R/r_files/refresh_mat_meta_statistics_poweredge.R
@@ -0,0 +1,12 @@
+library(redditor)
+
+refresh <- function() {
+  con <- postgres_connector(POSTGRES_HOST = Sys.getenv('POWEREDGE'))
+  on.exit(dbDisconnect(con))
+  dbExecute(
+    conn = con,
+    statement = read_file("../../sql/materialized_views/refresh_mat_meta_statistics.sql")
+  )
+}
+
+refresh()
