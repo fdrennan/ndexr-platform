0891ea80ceda4db2bf49967f031179797fe0d96c
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-05 18:08:19 -0600

    Add urls summary

--- airflow/airflower/scripts/R/r_files/refresh_urls_summary.R
+++ airflow/airflower/scripts/R/r_files/refresh_urls_summary.R
@@ -5,7 +5,7 @@ refresh <- function() {
   on.exit(dbDisconnect(con))
   dbExecute(
     conn = con,
-    statement = read_file("../../sql/materialized_views/refresh_urls_count_by_day.sql")
+    statement = read_file("../../sql/materialized_views/refresh_urls_summary.sql")
   )
 }
 
