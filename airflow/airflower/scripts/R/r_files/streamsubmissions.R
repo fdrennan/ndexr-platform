5a3bc045059399bb83a59e18a31074afd93775cc
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-06 18:34:39 -0600

    Update readme

--- airflow/airflower/scripts/R/r_files/streamsubmissions.R
+++ airflow/airflower/scripts/R/r_files/streamsubmissions.R
@@ -9,7 +9,7 @@ while (TRUE) {
   con <- postgres_connector()
   tryCatch(
     {
-      gather_submissions(con = con, reddit_con = reddit_con, sleep_time = 2)
+      gather_submissions(con = con, reddit_con = reddit_con, sleep_time = 3)
     },
     error = function(e) {
       send_message(glue("{str_sub(as.character(e), 1, 100)}"))
