223a7eff2349f71099730b3b64cb5d27b8fd9202
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-08 10:48:59 -0600

    Update sleep time to 60 seconds

--- airflow/airflower/scripts/R/r_files/streamsubmissions.R
+++ airflow/airflower/scripts/R/r_files/streamsubmissions.R
@@ -9,10 +9,10 @@ while (TRUE) {
   con <- postgres_connector()
   tryCatch(
     {
-      gather_submissions(con = con, reddit_con = reddit_con, forced_sleep_time = 60)
+      gather_submissions(con = con, reddit_con = reddit_con, forced_sleep_time = 30)
     },
     error = function(e) {
-      Sys.sleep(5)
+      Sys.sleep(60*5)
       send_message(glue("{str_sub(as.character(e), 1, 100)}"))
     }
   )
