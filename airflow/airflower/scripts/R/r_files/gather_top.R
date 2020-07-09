0f17ad26f4c9f58b8386167410c4f6548657d839
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-08 23:18:15 -0600

    Add ability to grab the top submissions each hour

--- airflow/airflower/scripts/R/r_files/gather_top.R
+++ airflow/airflower/scripts/R/r_files/gather_top.R
@@ -2,5 +2,5 @@ library(redditor)
 
 reddit_con <- reddit_connector()
 con <- postgres_connector()
-top_submissions <- get_submission(reddit = reddit_con, name = "all", limit = 1000L, type = "top")
+top_submissions <- get_submission(reddit = reddit_con, name = "all", limit = 1000L, type = "hot")
 dbxUpsert(con, "submissions_top", top_submissions, where_cols = c("submission_key"))
