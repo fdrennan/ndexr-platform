99110bd4f6066a2ccdbd1ec00c735a1234687403
Freddy Drennan <drennanfreddy@gmail.com>
2020-07-12 07:37:01 -0600

    Create connections for Python

--- py_redditor/scratch.py
+++ py_redditor/scratch.py
@@ -2,10 +2,13 @@ from redditor.subreddits import get_subreddit
 from dotenv import load_dotenv
 import praw
 import os
-
+import psycopg2
+import pandas as pd
+import plotly.express as px
 
 load_dotenv(verbose=True)
 
+# Grab Some Reddit Data
 reddit = praw.Reddit(client_id=os.getenv("REDDIT_CLIENT"),
                      client_secret=os.getenv("REDDIT_AUTH"),
                      user_agent=os.getenv("USER_AGENT"))
@@ -13,3 +16,20 @@ reddit = praw.Reddit(client_id=os.getenv("REDDIT_CLIENT"),
 subreddits = get_subreddit(reddit, 'politics',  10, 'hot')
 
 print(subreddits)
+
+# Create a connection
+conn = psycopg2.connect(host=os.getenv("POSTGRES_HOST"),
+                        database=os.getenv("POSTGRES_DB"),
+                        user=os.getenv("POSTGRES_USER"),
+                        password=os.getenv("POSTGRES_PASSWORD"))
+
+sql = "select date_trunc('hour', created_utc::timestamptz) as time_hour, count(*) as n_observations " \
+      "from submissions " \
+      "where lower(selftext) like '%wayfair%'" \
+      " by time_hour;"
+
+dat = pd.read_sql(sql, conn)\
+    .sort_values(by=['time_hour'])
+
+fig = px.line(dat, x='time_hour', y='n_observations')
+fig.show()
