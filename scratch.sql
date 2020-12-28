3381e0f48da69d54acda390de331df0283f68831
Freddy Drennan <drennanfreddy@gmail.com>
2020-12-27 23:12:28 -0700

    update

--- scratch.sql
+++ scratch.sql
@@ -1,2 +1,28 @@
-select count(*)
-from detect_roze
+
+create view roze_score as (
+    with score_table as (
+            select count(*) as cnt, 'true' as label
+            from roze
+            where detected_roze = 'true'
+            union
+            select count(*) as cnt, 'false' as label
+            from roze
+            where detected_roze = 'false'
+            union
+            select count(*) as cnt, 'null' as label
+            from roze
+            where detected_roze is null
+            union
+            select count(*) as cnt, 'total' as label
+            from roze
+    )
+
+    select *
+    from score_table
+    order by cnt desc
+);
+
+
+select *
+from roze
+where detected_roze is null
